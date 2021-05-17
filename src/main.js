d99d4454340f91ed83162a67c0a5c31fc2dc2bf1
Santiago Bernal <exactlimon@gmail.com>
2021-05-17 18:09:50 -0500

    refactor to a single html file

--- src/main.js
+++ src/main.js
@@ -5,9 +5,34 @@
 const saveName = () => {
   const nombreUsuario =  document.getElementById("name").value === null ? 'personita': document.getElementById("name").value;
   localStorage.setItem("nombre", nombreUsuario);
+
+  document.getElementById('intro').style.display = 'none';
+  document.getElementById('options').style.display = 'block';
+
+  getName();
 }
 
 const getName = () => {
   const nombreUsuario =  localStorage.getItem("nombre") === null ? 'personita': localStorage.getItem("nombre");
-  return document.getElementById("name").value=`${nombreUsuario}`;
+  document.getElementById("name").value=`${nombreUsuario}`;
+  document.getElementById("username").textContent =`${nombreUsuario}`;
+}
+
+const intro = () => {
+  document.getElementById('intro').style.display = 'block';
+  document.getElementById('options').style.display = 'none';
+  document.getElementById('cup').style.display = 'none';
+  document.getElementById('stadium').style.display = 'none';
+}
+
+const showCup = () => {
+  document.getElementById('options').style.display = 'none';
+  document.getElementById('stadium').style.display = 'none';
+  document.getElementById('cup').style.display = 'block';
+}
+
+const showStadium = () => {
+  document.getElementById('options').style.display = 'none';
+  document.getElementById('stadium').style.display = 'block';
+  document.getElementById('cup').style.display = 'none';
 }
