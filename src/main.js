5d5dd26806ba018e4472c15dfca3a525684869bb
Santiago Bernal <exactlimon@gmail.com>
2021-05-17 20:08:49 -0500

    cup Scores

--- src/main.js
+++ src/main.js
@@ -46,9 +46,62 @@ const clearRadioButtons = () => {
   Array.from( document.querySelectorAll('input[name="p6"]:checked'), input => input.checked = false );
 }
 
+const validateRadioButtons = (groupname) => {
+	for(var i=0;i<groupname.length;i++){
+    if(groupname[i].checked){
+      return true;
+    }
+  }
+  return false;
+}
+
+const getCupScore = () => {
+
+  if(!validateRadioButtons(document.getElementsByName('p1')) ||
+  !validateRadioButtons(document.getElementsByName('p2'))  ||
+  !validateRadioButtons(document.getElementsByName('p3'))){
+    alert('hay preguntas sin responder');
+  }else{
+    const answer1 = document.querySelector('input[name="p1"]:checked').value;
+    const answer2 = document.querySelector('input[name="p2"]:checked').value;
+    const answer3 = document.querySelector('input[name="p3"]:checked').value;
+
+    const answers = [2, 1, 3];
+    let score = 0;
+    if(answer1 == answers[0]){
+      score++;
+    }
+    if(answer2 == answers[1]){
+      score++;
+    }
+    if(answer3 == answers[2]){
+      score++;
+    }
+
+    switch (score) {
+      case 0:
+        alert('0 respuestas, sigue intentando');
+      case 1:
+        alert('1 respuesta correcta, conoces algo');
+        break;
+      case 2:
+        alert('2 respuestas, vas por buen camino');
+        break;
+      case 3:
+        alert('3 respuestas, así se hace!');
+        break;
+    }
+  }
+}
+
 const saveNameBtn = document.getElementById('saveName');
 saveNameBtn.addEventListener('click', () => {
   saveName();
+});
+
+const cupScoreBtn = document.getElementById('submitCup');
+cupScoreBtn.addEventListener('click', () => {
+  getCupScore();
 });
 
 const introBtn = document.getElementsByClassName('intro');
